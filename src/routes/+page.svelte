c329d42f5986d46331da3e2ba08bad62331a5db1
Collin Smallegan <cmsmallegan@comsma.com>
2023-06-01 12:53:08 -0500

    hello code

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="min-h-screen grid grid-cols-1 grid-rows-3 bg-gray-800 lg:grid-cols-3">
+  <div class="col-start-1 row-start-2 col-span-1 row-span-1">
+    <h1 class="mx-auto text-white font-bold text-[2rem] border-t-4 border-white w-fit pr-[40px] pt-[8px]">
+      Scotcheroo
+    </h1>
+  </div>
+</div>
